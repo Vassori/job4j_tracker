63bfe955f8c9136441c11b1fb01a73083f9efdd4
Vassori <o.komissarov.job@gmail.com>
2022-08-28 19:49:38 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,14 +1,30 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "meow - meow";
-        return voice;
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + ", " + this.food);
+    }
+
+    public void eat(String food) {
+        this.food = food;
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says: " + say);
+        System.out.println("There are gav's nick and food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("cutlet");
+        gav.show();
+        System.out.println("There are black's nick and food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
