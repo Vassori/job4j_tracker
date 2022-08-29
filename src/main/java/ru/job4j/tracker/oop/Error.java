1c4a7cb3cab3646d13ed0c964c7afe8877c9fb7d
Vassori <o.komissarov.job@gmail.com>
2022-08-29 09:31:21 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Activity : " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error active = new Error(true, 3, "Active.");
+        active.printInfo();
+        Error inactive = new Error(false, 0, "Inactive.");
+        inactive.printInfo();
+    }
+}
