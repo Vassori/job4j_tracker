9d47edd952436c185be069d1446d32a038315501
Vassori <o.komissarov.job@gmail.com>
2022-09-06 12:36:09 +0300

    7. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,6 @@ public class StartUI {
         Item item = new Item();
         String createdFormat = item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss"));
         System.out.println(createdFormat);
+
     }
 }
