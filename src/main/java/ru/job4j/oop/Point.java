d8d7a6dae2613b50c999a7605a69290b946db2b1
Vassori <o.komissarov.job@gmail.com>
2024-09-22 21:15:36 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642 #338500]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -19,12 +19,12 @@ public class Point {
         this.z = z;
     }
 
-    public double distance(Point another) {
-        return sqrt(pow(this.x - another.x, 2) + pow(this.y - another.y, 2));
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
-    public double distance3d(Point another) {
-        return sqrt(pow(this.x - another.x, 2) + pow(this.y - another.y, 2) + pow(this.z - another.z, 2));
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
     public static void main(String[] args) {
