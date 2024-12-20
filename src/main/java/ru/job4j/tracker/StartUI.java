e4424cc34188a78f5f6faddd62af8ae1e7893406
Vassori <o.komissarov.job@gmail.com>
2024-12-20 06:34:28 +0300

    6.1. Разрыв зависимости StartUI от Scanner. [#181778 #345796]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -45,7 +45,7 @@ public class StartUI {
 
     public static void findItemById(Input input, Tracker tracker) {
         System.out.println("=== Find item by id ===");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
+        int id = input.askInt("Enter id: ");
         Item item = tracker.findById(id);
         if (item != null) {
             System.out.println(item);
