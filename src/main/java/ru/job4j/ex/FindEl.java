49c5007fe3811529fd008480756b12360050729c
Vassori <o.komissarov.job@gmail.com>
2024-12-29 22:01:49 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                result = index;
+                break;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element is not found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] workingWeek = {"Mon", "Tue", "Wed", "Thu", "Fri"};
+        try {
+            indexOf(workingWeek, "Sun");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
