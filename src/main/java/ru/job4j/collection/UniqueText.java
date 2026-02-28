f678f55253e040ec1da3c83373ec3b85bee3be3b
Vassori <o.komissarov.job@gmail.com>
2026-02-28 21:00:49 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String orig : origin) {
+            check.add(orig);
+        }
+        for (String txt : text) {
+            if (!check.contains(txt)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
