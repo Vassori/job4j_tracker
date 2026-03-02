122c73025e393b278acf920234980aa03d85d0de
Vassori <o.komissarov.job@gmail.com>
2026-03-02 22:27:50 +0300

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,39 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder builder = new StringBuilder();
+        int size = evenElements.size();
+        for (int index = 0; index < size; index++) {
+            if (index % 2 == 0) {
+                builder.append(evenElements.poll());
+                evenElements.poll();
+            }
+        }
+        return builder.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder builder = new StringBuilder();
+        int size = descendingElements.size();
+        for (int index = 0; index < size; index++) {
+            builder.append(descendingElements.pollLast());
+        }
+        return builder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
