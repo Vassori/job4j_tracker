134b75dfd4d05d74b8e7806a5fbdb68015896f4a
Vassori <o.komissarov.job@gmail.com>
2026-03-03 19:39:50 +0300

    0. Map, HashMap [#212652 #559111]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,8 @@ public class UsageMap {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("vassori.mail@gmail.com", "Oleg Komissarov");
+        map.put("vassori.mail@gmail.com", "Komissarov Oleg");
+        map.put("gaku.jp@gmail.com", "コミッサロフ・オレグ");
         for (String key : map.keySet()) {
             String name = map.get(key);
             System.out.printf("Email: %s = Name: %s\n", key, name);
