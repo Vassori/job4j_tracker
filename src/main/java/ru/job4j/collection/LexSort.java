74e0aadf1e14d9117186da598fdc9fa96280ff33
Vassori <o.komissarov.job@gmail.com>
2026-03-11 08:45:50 +0300

    4. Сортировка номера [#221284]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -8,18 +8,8 @@ public class LexSort implements Comparator<String> {
     public int compare(String s1, String s2) {
         String[] array1 = s1.split("\\.");
         String[] array2 = s2.split("\\.");
-        int minSize = Math.min(array1.length, array2.length);
-        for (int index = 0; index < minSize; index++) {
-            String str1 = array1[index];
-            String str2 = array2[index];
-            if (Character.isDigit(str1.charAt(0)) && Character.isDigit(str2.charAt(0))) {
-                int num1 = Integer.parseInt(str1);
-                int num2 = Integer.parseInt(str2);
-                if (num1 != num2) {
-                    return Integer.compare(num1, num2);
-                }
-            }
-        }
-        return Integer.compare(s1.length(), s2.length());
+        int elArray1 = Integer.parseInt(array1[0]);
+        int elArray2 = Integer.parseInt(array2[0]);
+        return Integer.compare(elArray1, elArray2);
     }
 }
